640da31946356475d4d82433c26cae9a4760d6af
shani bider <shanibider@gmail.com>
2023-12-17 20:14:59 +0200

    added some code for blog ejs project

--- Backend/4.5 blog ejs website/app.js
+++ Backend/4.5 blog ejs website/app.js
@@ -1,30 +1,44 @@
-//Work plan- npm init -y, npm i express, write server including "/", "/contact", "about" endopints, nodemon index.js.
+//Work plan- npm init -y, npm i express, write server including "/", "/contact", "about" endopints, nodemon app.js.
 import express from "express";
 import bodyParser from "body-parser"; // Middleware
 import ejs from "ejs";
 import _ from "lodash";
+import nodemailer from "nodemailer";
+import path from "path";
+import Swal from 'sweetalert2/dist/sweetalert2.all.min.js';
+
 
 const homeStartingContent = "Hi Everyone.";
 const aboutContent = "I'm Shani, soptware developer.";
 const contactContent = "Lwt's keep in touch.";
+const notification = "";
 
-// EXPRESS:
+/*
+Creating the application structure, including routes, views, and static files.
+Setting up the Express.js server and defining the necessary routes.
+*/
+
+// Express.js server:
 const app = express();
 const port = 3000;
 
 app.set('view engine', 'ejs');
 
+// Body parser middleware
 app.use(bodyParser.urlencoded({ extended: true }));
+// Server static files
 app.use(express.static("public"));
 
 let posts = [];
 
 
-// the client ask for a reaource- the homepage
+// defining the necessary routes:
+
+// the client ask for a reaource- the homepage.
 app.get("/", function(req, res){
   res.render("home", {
-    startingContent: homeStartingContent,
-    posts: posts
+    startingContent: homeStartingContent, 
+    posts: posts, // Pass the posts array to home.ejs
     });
 });
 
@@ -35,31 +49,118 @@ app.get("/about", function(req, res){
 
 // GET contact:
 app.get("/contact", function(req, res){
-  res.render("contact", {contactContent: contactContent});
+  res.render("contact", {contactContent: contactContent, notification: notification});  // render the contact page and pass the contactContent variable to it 
 });
 
 
-// set up the "/compose" route
+// GET and POST compose:
 app.get("/compose", function(req, res){
   res.render("compose");
 });
+// post viewing feature- displaying all the posts on the home page
 app.post("/compose", function(req, res){
-    const post = {title: req.body.postTitle, content: req.body.postBody};
+    const post = {subject:req.body.postSubject, title: req.body.postTitle, content: req.body.postBody};
   posts.push(post);   //pushing the post into the posts array
   res.redirect("/");
 });
 
-// route to handle displaying a single post when the user clicks on Read More
+
+// displaying a single post when the user clicks on Read More. find a post with a title matching the requested post name and then renders a template with that post (post.ejs).
+// defining a route using app.get for handling GET requests to the path "/posts/:postName", The ":postName" part in the route is a route parameter, and it allows you to capture the value specified in the URL.
 app.get("/posts/:postName", function(req, res){
   const requestedTitle = _.lowerCase(req.params.postName);
 
-  posts.forEach(function(post){
-    const storedTitle = _.lowerCase(post.title);
-    if (storedTitle === requestedTitle) {
-            res.render("post", {title: post.title, content: post.content});
+  posts.forEach (function (post){
+    const storedTitle = _.lowerCase(post.title);    //converts to lowercase 
+    if (storedTitle === requestedTitle) {     // checks if the stored title matches the requested title
+            res.render("post", {subject: post.subject, title: post.title, content: post.content});    //If there is a match, it renders the "post" template, passing information about the post (subject, title, content) to be displayed.
     }
   });
 });
+
+
+
+// Edit post route - display the form to edit the post
+app.get("/edit/:postName", function (req, res) {
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(function (post) {
+    const storedTitle = _.lowerCase(post.title);
+    if (storedTitle === requestedTitle) {
+      res.render("edit", { post: post });
+    }
+  });
+});
+
+// Update post route - handle the form submission to update the post
+app.post("/edit/:postName", function (req, res) {
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  posts.forEach(function (post, index) {
+    const storedTitle = _.lowerCase(post.title);
+    if (storedTitle === requestedTitle) {
+      // Update the post with the new data
+      posts[index] = { subject: req.body.postSubject, title: req.body.postTitle, content: req.body.postBody };
+      res.redirect("/posts/" + _.lowerCase(req.body.postTitle));    //redirect to the post page
+    }
+  });
+});
+
+// Delete post route - handle post deletion
+app.get("/delete/:postName", function (req, res) {
+  const requestedTitle = _.lowerCase(req.params.postName);
+
+  // Filter out the post to be deleted
+  posts = posts.filter(function (post) {
+    return _.lowerCase(post.title) !== requestedTitle;    //return all the posts that don't match the requested title
+  });
+
+  res.redirect("/");
+});
+
+
+
+// POST request for handling the form submission
+app.post("/contact", function (req, res) {
+  // Extract form data
+  const name = req.body.name;
+  const email = req.body.email;
+  const message = req.body.message;
+
+  // Nodemailer configuration
+  const transporter = nodemailer.createTransport({
+    service: "gmail",
+    auth: {
+      user: "shanibider@gmail.com", // 
+      pass: "frbl yjwk gqvd prwa", // "App Password"- generated app password (provided by gmail)
+    },
+  });
+
+  // Email options
+  const mailOptions = {
+    from: email,
+    to: "shanibider@gmail.com", 
+    subject: `New Message from ${name}`,
+    text: `${message} \n\n Email sent from: ${email}`,  };
+
+  // Send the email
+  transporter.sendMail(mailOptions, function (error, info) {
+    if (error) {
+      console.error(error);
+      res.redirect("/contact?notification=Error: Unable to send the message.");      
+    } else {
+      console.log("Email sent: " + info.response);
+    
+     // Show SweetAlert for success
+     const successNotification = 'Email sent successfully!';
+     res.render("contact", { contactContent: contactContent, notification: successNotification });
+     res.redirect("/"); // Redirect to home page
+
+    }
+  });
+});
+
+
 
 
 app.listen(port, () => {
