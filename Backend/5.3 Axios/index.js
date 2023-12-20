d18ed5603c28f2bc6ca4cfd7a0a3eab7bf671efe
shani bider <shanibider@gmail.com>
2023-12-20 21:37:14 +0200

    add JSON and AXIOS projects

--- Backend/5.3 Axios/index.js
+++ Backend/5.3 Axios/index.js
@@ -5,17 +5,21 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
+let data;
+
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
-// Step 1: Make sure that when a user visits the home page,
-//   it shows a random activity.You will need to check the format of the
-//   JSON data from response.data and edit the index.ejs file accordingly.
+// Step 1: Make sure that when ia user visits the home page,
+//   it shows a random activity.
+//   You will need to check the format of the JSON data from
+//   response.data and edit the index.ejs file accordingly.
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
     const result = response.data;
     res.render("index.ejs", { data: result });
+ 
   } catch (error) {
     console.error("Failed to make request:", error.message);
     res.render("index.ejs", {
@@ -24,18 +28,43 @@ app.get("/", async (req, res) => {
   }
 });
 
+
+
+
+
+
 app.post("/", async (req, res) => {
   console.log(req.body);
-
   // Step 2: Play around with the drop downs and see what gets logged.
-  // Use axios to make an API request to the /filter endpoint. Making
-  // sure you're passing both the type and participants queries.
+  // Use axios to make an API request to the /filter endpoint. 
+  // Making sure you're passing both the type and participants queries.
   // Render the index.ejs file with a single *random* activity that comes back
   // from the API request.
   // Step 3: If you get a 404 error (resource not found) from the API request.
   // Pass an error to the index.ejs to tell the user:
   // "No activities that match your criteria."
+  try {
+console.log(req.body);
+    const response = await axios.get ("https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}");
+    const result = response.data;
+    console.log(result);
+
+    //res.render ("index.ejs", {data: result[]});
+
+
+
+  } catch (error) {
+    console.error("Failed to make request:", error.message);
+    res.render("index.ejs", {
+      error:  "No activities that match your criteria.",
+    });
+  }
 });
+
+
+
+
+
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
