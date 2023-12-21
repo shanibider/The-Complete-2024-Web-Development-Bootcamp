6ebd934f0c698f8b4ef3ab34c707f9efec71e197
shani bider <shanibider@gmail.com>
2023-12-21 19:36:54 +0200

    add JSON project

--- Backend/5.2 JSON/index.js
+++ Backend/5.2 JSON/index.js
@@ -1,3 +1,6 @@
+// JSON is JavaScript Object Notation
+// JSON is a format for storing and transporting data. JSON is often used when data is sent from a server to a web page. JSON uses JavaScript syntax, but the JSON format is text only.
+
 import express from "express";
 import bodyParser from "body-parser";
 
@@ -12,15 +15,48 @@ const recipeJSON =
 app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ extended: true }));
 
+let data;
+
+// handle HTTP GET requests to the root ("/"), renders "index.ejs", second parameter is an object {recipe: data}, data is a variable containing recipe information.
 app.get("/", (req, res) => {
-  res.render("index.ejs");
+  res.render("index.ejs", { recipe: data });  
 });
 
+/* handle POST requests to "/recipe" endpoint. uses switch to determine the choice made by the user based on 'req.body.choice'.
+index.ejs: <form action="/recipe" method="POST" class="buttons">
+           <button type="submit" value="chicken" name="choice">🍗</button>
+Depending on the choice, it sets the data variable with the corresponding recipe data (recipeJSON is a JSON string containing an array of recipes).
+*/
 app.post("/recipe", (req, res) => {
   //Step 3: Write your code here to make this behave like the solution website.
-  //Step 4: Add code to views/index.ejs to use the recieved recipe object.
+  //Step 4: Add code to index.ejs to use the recieved recipe object.
+switch(req.body.choice){
+  case "chicken":
+    // turning the JSON string into a JS object (like unbuilt closet to built closet).
+    // and depending on the user choice, we pick the item out of that json, just as if it was items in js array ([0], [1], [2]...).
+    data = JSON.parse(recipeJSON)[0];   //set data, and sent over the relevant piece of data with the res.render above.
+    break;
+  case "beef":
+    data = JSON.parse(recipeJSON)[1];
+    break;
+  case "fish":
+    data = JSON.parse(recipeJSON)[2];
+    break;
+}
+  res.redirect("/");  
 });
+
+
+/*
+Explanation for res.redirect("/") in the POST route:
+
+When the user visits the root ("/") using a GET request (e.g., by entering the site or refreshing the page), the server sends the "index.ejs" template along with the current recipe data to the client for rendering.
+When the user submits the form on the page (using the buttons with different values), a POST request is sent to "/recipe". The server processes this request, updates the data variable based on the user's choice, and then redirects the user back to the root ("/").
+The GET route for the root ("/") is then triggered again, and the updated recipe data is sent to the client, allowing the template to be re-rendered with the new recipe information.
+This approach essentially uses a server-side redirect to refresh the page with the updated recipe data after the user makes a choice.
+*/
 
 app.listen(port, () => {
   console.log(`Server running on port: ${port}`);
 });
+
