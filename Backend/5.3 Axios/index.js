77f2c975cf5ac73e93ba18feed1a9e3ac4d8a96b
shani bider <shanibider@gmail.com>
2023-12-21 16:53:11 +0200

    add AXIOS project

--- Backend/5.3 Axios/index.js
+++ Backend/5.3 Axios/index.js
@@ -5,9 +5,9 @@ import axios from "axios";
 const app = express();
 const port = 3000;
 
-let data;
 
 app.use(express.static("public"));
+// body-parser middleware set up
 app.use(bodyParser.urlencoded({ extended: true }));
 
 // Step 1: Make sure that when ia user visits the home page,
@@ -17,7 +17,10 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.get("/", async (req, res) => {
   try {
     const response = await axios.get("https://bored-api.appbrewery.com/random");
-    const result = response.data;
+    console.log("Response object: ", response);   // response is an object including lots of info includint the data (object with the activity info)
+    const result = response.data;         // extract the data from the response object
+    console.log("Result (random activity): ", result);
+
     res.render("index.ejs", { data: result });
  
   } catch (error) {
@@ -28,13 +31,7 @@ app.get("/", async (req, res) => {
   }
 });
 
-
-
-
-
-
 app.post("/", async (req, res) => {
-  console.log(req.body);
   // Step 2: Play around with the drop downs and see what gets logged.
   // Use axios to make an API request to the /filter endpoint. 
   // Making sure you're passing both the type and participants queries.
@@ -44,24 +41,24 @@ app.post("/", async (req, res) => {
   // Pass an error to the index.ejs to tell the user:
   // "No activities that match your criteria."
   try {
-console.log(req.body);
-    const response = await axios.get ("https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}");
+    console.log("req.body:", req.body);
+    const type = req.body.type;
+    const participants = req.body.participants;
+    // important to put ` ` around the url (and not " or ')
+    const response = await axios.get (`https://bored-api.appbrewery.com/filter?type=${type}&participants=${participants}`);
     const result = response.data;
-    console.log(result);
+    console.log("Result of post request (by user choice): ", result);
 
-    //res.render ("index.ejs", {data: result[]});
-
-
+    res.render ("index.ejs", { data: result [Math.floor(Math.random() * result.length)] ,});  // random activity from the array of activities that match the criteria
+ 
 
   } catch (error) {
-    console.error("Failed to make request:", error.message);
+    console.error("Failed to make request:", error.message);    //log with , not +
     res.render("index.ejs", {
       error:  "No activities that match your criteria.",
     });
   }
 });
-
-
 
 
 
