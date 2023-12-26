fc1149ccf512cb3fe6db43bda9141e3de474406a
shani bider <shanibider@gmail.com>
2023-12-26 14:37:02 +0200

    add REST APIs project

--- Backend/5.4 API Authentication/index.js
+++ Backend/5.4 API Authentication/index.js
@@ -5,7 +5,7 @@ const app = express();
 const port = 3000;
 const API_URL = "https://secrets-api.appbrewery.com/";
 
-//TODO 1: Fill in your values for the 3 types of auth.
+//TODO 1: Fill in your values for the 3 types of auth (from postman from txt file)
 const yourUsername = "shanib";
 const yourPassword = "123";
 const yourAPIKey = "59db4d87-5a60-411d-8818-28e806bc673e";
@@ -18,7 +18,7 @@ app.get("/", (req, res) => {
 app.get("/noAuth", (req, res) => {
   //TODO 2: Use axios to hit up the /random endpoint
   //The data you get back should be sent to the ejs file as "content"
-  const result = axios.get (API_URL + "/random");
+  const result = axios.get (API_URL + "/random");     // axios get request 
   res.render("index.ejs", { content: JSON.stringify(result.data) });
   //using JSON.stringify to turn the JS object from axios into a string.
 });
@@ -26,7 +26,7 @@ app.get("/noAuth", (req, res) => {
 app.get("/basicAuth", (req, res) => {
   //TODO 3: Write your code here to hit up the /all endpoint
   //Specify that you only want the secrets from page 2
-  const result = axios.get (API_URL + "/all?page=2", {}, {
+  const result = axios.get (API_URL + "/all?page=2", {}, {        // axios get request of basic auth
     auth: {
       username: yourUsername,
       password: yourPassword,
@@ -45,23 +45,28 @@ app.get("/basicAuth", (req, res) => {
   */
 });
 
+
+
 app.get("/apiKey", (req, res) => {
   //TODO 4: Write your code here to hit up the /filter endpoint
   //Filter for all secrets with an embarassment score of 5 or greater
   //HINT: You need to provide a query parameter of apiKey in the request.
-  const result = axios.get (API_URL + "/filter", {
+  const result = axios.get (API_URL + "/filter", {      // axios get request with api authorazition
     params: {
       score: 5,
       apiKey: yourAPIKey,
     },
   });
+  
   res.render("index.ejs", { content: JSON.stringify(result.data) });
 });
+
+
 
 app.get("/bearerToken", (req, res) => {
   //TODO 5: Write your code here to hit up the /secrets/{id} endpoint
   //and get the secret with id of 42
-  const result = axios.get (API_URL + "/secrets/42", {
+  const result = axios.get (API_URL + "/secrets/42", {      // axios get request with bearer token auth
     headers: {
       Authorization: `Bearer ${yourBearerToken}`,
     },
@@ -76,6 +81,7 @@ app.get("/bearerToken", (req, res) => {
   });
   */
 });
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
--- Backend/5.5 REST APIs/index.js
+++ Backend/5.5 REST APIs/index.js
@@ -1,18 +1,14 @@
-// Making all range of requests- GET, POST, PUT, PATCH and DELETE API requests using axios.
+// Making all range of requests- GET, POST, PUT, PATCH and DELETE API requests using axios. With bearer token authentication.
 
 /* i saw how to make a GET request using axios in the previous project.
 to this request i can chain a .then() method to handle the response and a .catch() method to handle the error or .finally.
 axios.get ("URL", {
-         params: {        //here we can use params/auth/data/headers
+         params: {        //here we can use params/auth/data/headers (config)
              score: 5,
              apiKey: "abc",
            },
          });
  */
-
-
-
-
 
 import express from "express";
 import axios from "axios";
@@ -28,46 +24,102 @@ const API_URL = "https://secrets-api.appbrewery.com";
 // https://secrets-api.appbrewery.com/
 
 //TODO 1: Add your own bearer token from the previous lesson.
-const yourBearerToken = "";
+const yourBearerToken = "ed1caba6-ea18-4cc4-a719-ced667576b14";
 const config = {
   headers: { Authorization: `Bearer ${yourBearerToken}` },
 };
 
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ extended: true }));   
 
+// get request to the root route
 app.get("/", (req, res) => {
   res.render("index.ejs", { content: "Waiting for data..." });
 });
 
+// axios get request 
 app.post("/get-secret", async (req, res) => {
-  const searchId = req.body.id;
+  const searchId = req.body.id;   // id is "idInput" in index.ejs
   try {
+    // GET /secrets/{id} - Returns the secret with the specified ID. Bearer token authentication is required.
     const result = await axios.get(API_URL + "/secrets/" + searchId, config);
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  }   
+  catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
+});
+
+// axios post request
+app.post("/post-secret", async (req, res) => {
+
+  // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
+  // req.body is all that inside <form> in index.ejs
+  try {
+  // POST /secrets - Adds a new secret. 
+  const result = await axios.post (API_URL + "/secrets/", req.body, config);
+  res.render("index.ejs", { content: JSON.stringify(result.data) });
+}  
+catch (error) {
+  res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+}
+});
+
+
+
+// put request provides all the data it wants to update
+app.post("/put-secret", async (req, res) => {
+  const searchId = req.body.id;
+
+  // TODO 3: Use axios to PUT the data from req.body to the secrets api servers.  
+  // PUT /secrets/{id} - Updates the content of the secret with the specified ID.
+  try {
+    const result = await axios.put(
+      API_URL + "/secrets/" + searchId,
+      req.body,
+      config
+    );
     res.render("index.ejs", { content: JSON.stringify(result.data) });
   } catch (error) {
     res.render("index.ejs", { content: JSON.stringify(error.response.data) });
   }
 });
 
-app.post("/post-secret", async (req, res) => {
-  // TODO 2: Use axios to POST the data from req.body to the secrets api servers.
-});
 
-app.post("/put-secret", async (req, res) => {
-  const searchId = req.body.id;
-  // TODO 3: Use axios to PUT the data from req.body to the secrets api servers.
-});
-
+// patch- provide any data you want to update
 app.post("/patch-secret", async (req, res) => {
   const searchId = req.body.id;
   // TODO 4: Use axios to PATCH the data from req.body to the secrets api servers.
+  // PATCH /secrets/{id} - Partially updates the content of the secret with the specified ID.
+  try {
+    const result = await axios.patch(
+      API_URL + "/secrets/" + searchId,
+      req.body,
+      config
+    );
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
 });
 
+
+// takes only 2 parameters- url and config (as js object)
 app.post("/delete-secret", async (req, res) => {
   const searchId = req.body.id;
   // TODO 5: Use axios to DELETE the item with searchId from the secrets api servers.
+  // DELETE /secrets/{id} - Deletes the secret with the specified ID.
+  try {
+    const result = await axios.delete(API_URL + "/secrets/" + searchId, config);
+    res.render("index.ejs", { content: JSON.stringify(result.data) });
+  } catch (error) {
+    res.render("index.ejs", { content: JSON.stringify(error.response.data) });
+  }
 });
+
+
 
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
+
+
