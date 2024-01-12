9eb8e87e4a2896a46acc9809f062bbfad580b7ba
Shani Bider <shanibider@gmail.com>
2024-01-12 13:46:58 +0200

    add code to permalist project

--- Backend/8.6 Permalist Project/index.js
+++ Backend/8.6 Permalist Project/index.js
@@ -1,6 +1,12 @@
-// A permanent todo list (based on postgreSQL storage + pgAdmin) -
+// A permanent todo list - server and client side, db queries based on postgreSQL storage + pgAdmin -
 // include all CRUD actions. 
-// first - create permalist db in pgAdmin
+// Work flow - create new database in pgAdmin: "permalist", and table "items" with columns "id" and "title".
+
+/*
+"id"	"title"
+1	"Buy milk"
+2	"Go to the gym"
+*/
 
 import express from "express";
 import bodyParser from "body-parser";
@@ -33,13 +39,14 @@ app.get("/", async (req, res) => {
 
   try {
   const result = await db.query (
-    "SELECT * FROM items ORDER BY id ASC");
+    "SELECT * FROM items ORDER BY (id) ASC");   // ASC - ascending order
   items = result.rows;  
 
   res.render("index.ejs", {
     listTitle: "Today",
     listItems: items,
   });
+
 }  catch (err) {
   console.log(err);
 }
@@ -48,33 +55,51 @@ app.get("/", async (req, res) => {
 
 
 // add POST request
-app.post("/add", (req, res) => {
-  const item = req.body.newItem;
-  items.push({ title: item });
-  res.redirect("/");
+app.post("/add", async (req, res) => {
+  const item = req.body.newItem;    // name of the input field
+  //items.push ({ title: item });  
+  try{
+    await db.query (
+      "INSERT INTO items (title) VALUES ($1)", [item]);   // insert query
+      res.redirect ("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 
 
 // edit POST request
-app.post("/edit", (req, res) => {
-
-
-
+app.post("/edit", async (req, res) => {
+  const item = req.body.updatedItemTitle;    // item title
+  const id = req.body.updatedItemId;    // item id
+  try{
+    await db.query (
+      "UPDATE items SET title = $1 WHERE id = $2",
+      [item, id]
+    );
+      res.redirect ("/");
+  } catch (err) {
+    console.log(err);
+  }
 });
 
 
 
 
 // delete POST request
-app.post("/delete", (req, res) => {
-
-
-
+app.post("/delete", async (req, res) => {
+const id = req.vody.deleteItem;    // deleteItemId is the name of the input field in ejs file
+try{
+  await db.query (
+    "DELETE FROM items WHERE id = $1",
+    [id]
+  );
+    res.redirect ("/");
+} catch (err) {
+  console.log(err);
+}
 });
-
-
-
 
 
 
